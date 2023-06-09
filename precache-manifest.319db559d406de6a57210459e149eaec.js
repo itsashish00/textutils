f8006aaeb9ecec4b203e11b627bd6c298a7b0146
itsashish00 <ashishkanere05@gmail.com>
2023-06-09 13:40:55 +0530

    Updates

---
+++ precache-manifest.319db559d406de6a57210459e149eaec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02c7f8068897489f8230",
+    "url": "/textutils/static/css/main.bd5787df.chunk.css"
+  },
+  {
+    "revision": "02c7f8068897489f8230",
+    "url": "/textutils/static/js/main.02c7f806.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textutils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "91449b700f0ab8a2a413",
+    "url": "/textutils/static/js/runtime~main.91449b70.js"
+  },
+  {
+    "revision": "af4a4cc67eb3626d9e8c00cc71bd99c9",
+    "url": "/textutils/index.html"
+  }
+];
